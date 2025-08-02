e33708b8775da2d0447de4ff9deab48c17a54c10
MateoNeglia <mateo.neglia@gmail.com>
2025-08-02 13:39:52 -0300

    patch method added

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
